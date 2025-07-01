e7ad6cca87d2b7e890c0347971c633e9062a16b5
MohammedIndusVisionAI <tayibulla@indusvision.ai>
2025-07-01 15:46:19 +0530

    updated cad with brightnes

--- src/Viewer.js
+++ src/Viewer.js
@@ -1,104 +1,3 @@
-// import React, { useEffect, useState } from 'react'
-// import Unity, { UnityContent } from "react-unity-webgl";
-// import { makeStyles } from '@material-ui/core/styles';
-
-// const unityContent = new UnityContent(
-//     "Build/build.json",
-//     "Build/UnityLoader.js",
-//     {
-//         adjustOnWindowResize: true
-//     }
-// );
-
-// const useStyles = makeStyles((theme) => ({
-//     root: {
-//         width: "100%",
-//         height: "100%"
-//     },
-//     unityContent: {
-//         background: "white",
-//         width: "100%",
-//         height: "100%",
-//     },
-//     paper: {
-//         padding: theme.spacing(2),
-//         textAlign: 'center',
-//         color: theme.palette.text.secondary,
-//     },
-// }));
-
-
-
-// export default function Viewer(props) {
-//     console.log(props)
-//     const classes = useStyles();
-//     const [ready, setReady] = useState(false);
-//     const [fileName, setFileName] = useState(null);
-
-//     unityContent.on("Ready", () => {
-//         setReady(true)
-//         loadFile();
-
-//         if (typeof props.onReady == "function") props.onReady();
-//     }
-//     );
-
-//     unityContent.on("OnLoaded", () => {
-//         try {
-//             if (typeof props.onLoaded == "function") props.onLoaded();
-//         }
-//         catch{ }
-//     }
-//     );
-
-//     unityContent.on("OnError", () => {
-//         try {
-//             if (typeof props.onError == "function") props.onError();
-//         }
-//         catch{ }
-//     }
-//     );
-
-//     useEffect(() => {
-//         if (ready) loadFile()
-//     }, [props.file]);
-
-//     const loadFile = () => {
-//         try {
-//             if (props.file && typeof props.file == "object") {
-//                 var reader = new FileReader();
-//                 reader.onload = (function (file) {
-//                     return function (e) {
-//                         (window.filedata = window.filedata ? window.filedata : {})[file.name] = e.target.result;
-//                         unityContent.send("root", "FileUpload", file.name)
-//                         setFileName(file.name);
-//                     }
-//                 })(props.file);
-//                 reader.readAsArrayBuffer(props.file);
-//             }
-//             else if (typeof props.file == "string") {
-//                 unityContent.send("root", "Load", JSON.stringify({ file: props.file }))
-//                 setFileName(props.file);
-//             }
-//             else {
-//                 unityContent.send("root", "Clear");
-//                 setFileName("");
-//             }
-//         }
-//         catch (e) {
-//             console.log(e);
-//             if (typeof props.onError == "function") props.onError();
-//         }
-
-//     }
-// console.log(unityContent)
-//     return (
-//         <div className={classes.root} >
-//             <Unity unityContent={unityContent} height="100%" width="100%" className={classes.unityContent} />
-//         </div>
-//     )
-// }
-
 import React, { useEffect, useState } from 'react'
 import Unity, { UnityContent } from "react-unity-webgl";
 import { makeStyles } from '@material-ui/core/styles';
@@ -133,7 +32,13 @@ export default function Viewer(props) {
     const classes = useStyles();
     const [ready, setReady] = useState(false);
     const [fileName, setFileName] = useState(null);
-
+const [settings, setSettings] = useState({
+    brightness: 2.0,
+    contrast: 1.6,
+    saturation: 1.4,
+    ambientIntensity: 2.0,
+    directionalIntensity: 1.8,
+  });
     unityContent.on("Ready", () => {
         setReady(true)
         loadFile();
@@ -217,7 +122,8 @@ export default function Viewer(props) {
         if (ready) {
             const unityCanvas = document.querySelector('canvas');
             if (unityCanvas) {
-                unityCanvas.style.filter = 'brightness(1.4) contrast(1.1)';
+                  unityCanvas.style.filter = `brightness(${settings.brightness}) contrast(${settings.contrast}) saturate(${settings.saturation})`;
+                // unityCanvas.style.filter = 'brightness(1.4) contrast(1.1)';
             }
         }
     }, [ready]);
